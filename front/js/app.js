052c91d9ac6033c8e3ed267aaa0f2b2df9663793
steve <steve.taramasco@gmail.com>
2020-08-21 16:04:32 +0200

    architecture bootstrap

--- front/js/app.js
+++ front/js/app.js
@@ -3,10 +3,9 @@
 const request = new XMLHttpRequest();
 request.onreadystatechange = function () {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        
         response = JSON.parse(this.responseText)
-
         newArticle(response);
-        details(response);
     }
 }
 request.open('GET', 'http://localhost:3000/api/teddies', true);
@@ -16,12 +15,9 @@ request.send();
 // fonction contenu //
 
 function newArticle(elts) {
-    const section = document.querySelector('section');
-    const div_row = document.createElement('div');
 
-    div_row.classList.add("row");
-    section.appendChild(div_row);
-
+    const div_row = document.getElementById('content-top');
+    
     for (let i = 0; i < elts.length; i++) {
 
         const div_col = document.createElement('div');
@@ -30,31 +26,31 @@ function newArticle(elts) {
         const h2 = document.createElement('h2');
         const image = document.createElement('img');
         const price = document.createElement('p');
-        const link = document.createElement('a');
+        const button = document.createElement('a');
 
         h2.textContent = elts[i].name;
         price.textContent = elts[i].price + " €";
+        button.textContent = "En savoir plus";
 
-        div_col.classList.add("col-12", "col-md-6", "teddys");
-        article.classList.add("card");
+        article.classList.add("col-12", "col-md-5", "teddys", "m-3", "px-0", "card", "shadow");
         div.classList.add("card-body");
         h2.classList.add("card-title");
         price.classList.add("card-text");
+        button.classList.add("btn", "btn-info");
 
-        link.setAttribute("href", "produit.html");
-        link.classList.add('produit')
+        button.setAttribute("href", "./front/produit.html");
         image.setAttribute("src", elts[i].imageUrl);
         image.classList.add("card-image-top");
 
-        div_row.appendChild(div_col);
-        div_col.appendChild(article);
+        div_row.appendChild(article);
         article.appendChild(div);
-        article.appendChild(link);
-        link.appendChild(image)
+        article.appendChild(image)
         div.appendChild(h2);
         div.appendChild(price);
+        div.appendChild(button);
     }
 }
+/*
 
 function details(elts) {
 
@@ -72,3 +68,4 @@ function details(elts) {
         })
     }
 }
+*/
